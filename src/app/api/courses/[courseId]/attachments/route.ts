8477c8b50fb6a3e5f8f1733253448f219ce211c3
igor.znamensky <igor.znamensky@gmail.com>
2025-09-15 10:19:36 +0300

    Attachment Form (half) [POST vs PATCH]

--- src/app/api/courses/[courseId]/attachments/route.ts
+++ src/app/api/courses/[courseId]/attachments/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { auth } from '@clerk/nextjs/server';
 import { prisma } from '@/lib/db';
 
-export async function PATCH(request: Request, { params }: { params: { courseId: string } }) {
+export async function POST(request: Request, { params }: { params: { courseId: string } }) {
   try {
     const { userId } = await auth();
     const { courseId } = params;
